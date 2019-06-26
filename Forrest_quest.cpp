4db924602b93b88b03328e14e464e0e144d9e456
Grayson <barrowsgrax@gmail.com>
2019-06-26 19:52:56 -0400

    First Commit

---
+++ Forrest_quest.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{    
+      string user = "";
+      
+
+
+       cout << "/                                           \\ "<<endl; 
+       cout << "/                      Forest  Quest        \\  "<<endl;
+       cout << "/                                           \\  "<<endl;
+       cout << "/                                           \\  "<<endl;
+       cout << " please enter your username (no spaces) "<<endl;
+       cin >> user;
+       cout << "                               Loding user...              "<<endl;
+       cout << "       Loding world       "<<endl;
+
+
+
+
+
+                                                                    
+       
+
+}//end main        
+  
+      
+ 
+ 
+ 
